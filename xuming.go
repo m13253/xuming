b329e8ece4b6d483277963b4a0254d04c30810df
Star Brilliant <m13253@hotmail.com>
2016-04-26 17:23:59 +0800

    Initial commit

--- xuming.go
+++ xuming.go
@@ -9,9 +9,6 @@ import (
 	"time"
 )
 
-var life uint64
-var online uint64
-
 func main() {
 	prepareQuotes()
 	http.HandleFunc("/", redirectHandler)
@@ -25,15 +22,19 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "+1s", 302)
 }
 
+var life uint64
+var online int64
+var tmpl, _ = template.ParseFiles("template/index.html")
+
 func plus1sHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("template/index.html")
-	t.Execute(w, struct {
+	current := atomic.AddUint64(&life, 1)
+	tmpl.Execute(w, struct {
 		Life uint64
-		Online uint64
+		Online int64
 		Quote string
 	} {
 		Life: current,
-		Online: lastonline+1,
+		Online: online,
 		Quote: getQuote(),
 	})
 }
@@ -41,13 +42,14 @@ func plus1sHandler(w http.ResponseWriter, r *http.Request) {
 func plus1sHeartbeatHandler(w http.ResponseWriter, r *http.Request) {
 	current := atomic.AddUint64(&life, 1)
 	w.Header().Set("Content-Type", "application/json; encoding=UTF-8")
-	fmt.Fprintf(w, "{\"life\":%d,\"online\":%d}", current, lastonline)
+	fmt.Fprintf(w, "{\"life\":%d,\"online\":%d}", current, online)
 }
 
 func quoteHandler(w http.ResponseWriter, r *http.Request) {
-	atomic.AddUint64(&online, 1)
-	time.Sleep(9700 * time.Milisecond)
-	atomic.AddUint64(&online, -1)
 	w.Header().Set("Content-Type", "test/plain; encoding=UTF-8")
+	w.WriteHeader(200)
+	atomic.AddInt64(&online, 1)
+	time.Sleep(9700 * time.Millisecond)
+	atomic.AddInt64(&online, -1)
 	w.Write([]byte(getQuote()))
 }
